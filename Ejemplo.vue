9f08f44e0f30870ecb4ce3240c67b347d65cc336
P.HaroldEscobarVallejos <haroldescobarvallejosmrz@gmail.com>
2022-05-24 13:53:38 -0400

    practica 2

---
+++ Ejemplo.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div >    <!-- de ser necesario cambiar el fondo -->
+    <h1> Este en un titulo de prueba</h1> <!-- cambiar la fuente -->
+    <img src="" alt="">  
+    <!-- cambiar la fuente -->
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, aspernatur.</p> 
+    <i>Iconos</i>   <!-- cambiar el color de los iconos -->
+    <!-- los iconos se pueden modificar como si fueran texto -->
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sint repellat delectus!</p>
+    <button>    <!-- cambiar el color de los botones de ser necesario -->
+        boton de prueba
+    </button>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt quia modi sunt facere quod, aut voluptatibus.</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  /* 
+    los cambios pueden hacerse aqui.
+  */
+</style>
